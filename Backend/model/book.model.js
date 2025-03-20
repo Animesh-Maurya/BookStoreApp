7a25c8079066f839ddbe6a0910dfd1fe137315ad
dhruvordangi <dhruvdangi03@gmail.com>
2025-03-20 13:27:21 +0530

    all

--- Backend/model/book.model.js
+++ Backend/model/book.model.js
@@ -1,12 +1,43 @@
-import mongoose from "mongoose";
+import mongoose,{Schema} from "mongoose";
 
-const bookSchema = mongoose.Schema({
-    name:String,
-    price:Number,
-    category:String,
-    image: String,
-    title:String,
-})
-const Book = mongoose.model("Book",bookSchema);
+const bookSchema = Schema({
+    name:{
+        type:String,
+        required:true
+    },
+    price:{
+        type:Number,
+        required:true
+    },
+    category:{
+        type:String,
+    },
+    image: {
+        type:String,
+    },
+    title:{
+        type:String,
+        required:true
+    },
+    author:{
+        type:String,
+        required:true
+    },
+    File:
+    {
+        url: {
+        type: String,
+        required: [true, 'File URL is required'],
+        },
+        fileType: {
+        type: String,
+        enum: ['pdf', 'doc', 'docx', 'zip', 'other'],
+        default: 'other',
+        },
+    },
+},
+{timestamps: true}
 
-export default Book ;
+);
+export const Book = mongoose.model("Book",bookSchema);
+
--- Backend/model/user.model.js
+++ Backend/model/user.model.js
@@ -1,6 +1,6 @@
-import mongoose from "mongoose";
+import mongoose,{Schema} from "mongoose";
 
-const userSchema=mongoose.Schema({
+const userSchema=Schema({
     fullname:{
         type:String,
         require:true,
@@ -9,12 +9,28 @@ const userSchema=mongoose.Schema({
         type:String,
         require:true,
     },
+    role :{
+        type:String,
+        required:true,
+    },
     password:{
         type:String,
         require:true,
     },
+    cart:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Book'
+        }
+    ],
+    bought_books:[
+        {
+            type:Schema.Types.ObjectId,
+            ref:'Book'
+        }
+    ],
+},
+    {timestaps: true}
+);
 
-});
-
-const User=mongoose.model("User",userSchema);
-export default User ;
+export const User=mongoose.model("User",userSchema);
