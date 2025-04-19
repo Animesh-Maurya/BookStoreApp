ce376618eb2b6105e908ae5c31e35febb77d02f3
dhruvordangi <dhruvdangi03@gmail.com>
2025-04-19 17:11:47 +0530

    null

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -19,6 +19,7 @@ createRoot(document.getElementById("root")).render(
         <div className='dark:bg-slate-900 dark:text-white'>
           <App />
         </div>
+        
       </ChatProvider>
     
     </Auth0Provider>
